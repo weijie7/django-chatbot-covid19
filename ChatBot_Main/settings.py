5d7a993688504af8df86c0c3b32feda9978c2eb0
unknown <weijie7@live.com>
2020-03-16 22:27:26 +0800

    try another static

--- ChatBot_Main/settings.py
+++ ChatBot_Main/settings.py
@@ -15,10 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIR = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # take a list of static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 
 # Quick-start development settings - unsuitable for production
