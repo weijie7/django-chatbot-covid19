8115065b0bfc4b02978efc6b4f536bce42e05251
unknown <weijie7@live.com>
2020-04-07 17:16:55 +0800

    Version 3.2.1 - edited default time out for rd queue to 5000s

--- Run_generate_graph.py
+++ Run_generate_graph.py
@@ -2,6 +2,6 @@ from rq import Queue
 from worker import conn
 from Generate_graph import plot_it
 
-q = Queue(connection = conn)
+q = Queue(connection = conn, default_timeout=5000)
 comment = q.enqueue(plot_it)
 print(comment)
